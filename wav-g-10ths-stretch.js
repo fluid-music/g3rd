95115b6562bf26e7384258cccd38b95416622748
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-11-17 16:23:16 -0500

    replace .mp3 files with .wav files for non-streched samples

---
+++ wav-g-10ths-stretch.js
@@ -0,0 +1,146 @@
+module.exports = {
+  "01-A2-min-003-stretch.wav": {
+    "path": "wav-g-10ths-stretch/01-A2-min-003-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 711048,
+      "duration": 16.123537414965988
+    }
+  },
+  "02-B2-min-004-stretch.wav": {
+    "path": "wav-g-10ths-stretch/02-B2-min-004-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 540176,
+      "duration": 12.248888888888889
+    }
+  },
+  "03-C3-maj-004-stretch.wav": {
+    "path": "wav-g-10ths-stretch/03-C3-maj-004-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 534664,
+      "duration": 12.12390022675737
+    }
+  },
+  "04-D3-maj-004-stretch.wav": {
+    "path": "wav-g-10ths-stretch/04-D3-maj-004-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 556712,
+      "duration": 12.623854875283447
+    }
+  },
+  "05-E3-min-005-stretch.wav": {
+    "path": "wav-g-10ths-stretch/05-E3-min-005-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 578760,
+      "duration": 13.123809523809523
+    }
+  },
+  "06-Fs3-min-005-stretch.wav": {
+    "path": "wav-g-10ths-stretch/06-Fs3-min-005-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 595296,
+      "duration": 13.498775510204082
+    }
+  },
+  "07-G3-maj-002-stretch.wav": {
+    "path": "wav-g-10ths-stretch/07-G3-maj-002-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 997960,
+      "duration": 22.629478458049885
+    }
+  },
+  "08-A3-min-004-stretch.wav": {
+    "path": "wav-g-10ths-stretch/08-A3-min-004-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 540176,
+      "duration": 12.248888888888889
+    }
+  },
+  "09-A3-min-001-stretch.wav": {
+    "path": "wav-g-10ths-stretch/09-A3-min-001-stretch.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 1047280,
+      "duration": 23.74784580498866
+    }
+  }
+}
---
+++ wav-g-10ths.js
@@ -0,0 +1,786 @@
+module.exports = {
+  "A2-maj-001.wav": {
+    "path": "wav-g-10ths/A2-maj-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 174840,
+      "duration": 3.964625850340136
+    }
+  },
+  "A2-maj-002.wav": {
+    "path": "wav-g-10ths/A2-maj-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 85337,
+      "duration": 1.9350793650793652
+    }
+  },
+  "A2-min-001.wav": {
+    "path": "wav-g-10ths/A2-min-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 62115,
+      "duration": 1.4085034013605442
+    }
+  },
+  "A2-min-002.wav": {
+    "path": "wav-g-10ths/A2-min-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 129652,
+      "duration": 2.939954648526077
+    }
+  },
+  "A2-min-003.wav": {
+    "path": "wav-g-10ths/A2-min-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 88692,
+      "duration": 2.011156462585034
+    }
+  },
+  "A2-min-004.wav": {
+    "path": "wav-g-10ths/A2-min-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 70393,
+      "duration": 1.5962131519274376
+    }
+  },
+  "A3-min-001.wav": {
+    "path": "wav-g-10ths/A3-min-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 130392,
+      "duration": 2.956734693877551
+    }
+  },
+  "A3-min-002.wav": {
+    "path": "wav-g-10ths/A3-min-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 103607,
+      "duration": 2.3493650793650795
+    }
+  },
+  "A3-min-003.wav": {
+    "path": "wav-g-10ths/A3-min-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 64047,
+      "duration": 1.452312925170068
+    }
+  },
+  "A3-min-004.wav": {
+    "path": "wav-g-10ths/A3-min-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 67365,
+      "duration": 1.5275510204081633
+    }
+  },
+  "A3-min-005.wav": {
+    "path": "wav-g-10ths/A3-min-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 69510,
+      "duration": 1.5761904761904761
+    }
+  },
+  "B2-8va-000.wav": {
+    "path": "wav-g-10ths/B2-8va-000.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 73791,
+      "duration": 1.673265306122449
+    }
+  },
+  "B2-min-001.wav": {
+    "path": "wav-g-10ths/B2-min-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 98888,
+      "duration": 2.242358276643991
+    }
+  },
+  "B2-min-002.wav": {
+    "path": "wav-g-10ths/B2-min-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 90685,
+      "duration": 2.056349206349206
+    }
+  },
+  "B2-min-003.wav": {
+    "path": "wav-g-10ths/B2-min-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 58900,
+      "duration": 1.3356009070294785
+    }
+  },
+  "B2-min-004.wav": {
+    "path": "wav-g-10ths/B2-min-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 66944,
+      "duration": 1.5180045351473923
+    }
+  },
+  "B2-min-005.wav": {
+    "path": "wav-g-10ths/B2-min-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 65112,
+      "duration": 1.4764625850340136
+    }
+  },
+  "B2-min-006.wav": {
+    "path": "wav-g-10ths/B2-min-006.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 65945,
+      "duration": 1.4953514739229026
+    }
+  },
+  "C3-maj-001.wav": {
+    "path": "wav-g-10ths/C3-maj-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 110953,
+      "duration": 2.5159410430839
+    }
+  },
+  "C3-maj-002.wav": {
+    "path": "wav-g-10ths/C3-maj-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 99010,
+      "duration": 2.245124716553288
+    }
+  },
+  "C3-maj-003.wav": {
+    "path": "wav-g-10ths/C3-maj-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 62476,
+      "duration": 1.4166893424036282
+    }
+  },
+  "C3-maj-004.wav": {
+    "path": "wav-g-10ths/C3-maj-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 66305,
+      "duration": 1.503514739229025
+    }
+  },
+  "C3-maj-005.wav": {
+    "path": "wav-g-10ths/C3-maj-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 75542,
+      "duration": 1.7129705215419502
+    }
+  },
+  "C3-maj-006.wav": {
+    "path": "wav-g-10ths/C3-maj-006.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 82695,
+      "duration": 1.875170068027211
+    }
+  },
+  "D2-8va.wav": {
+    "path": "wav-g-10ths/D2-8va.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 82529,
+      "duration": 1.87140589569161
+    }
+  },
+  "D3-maj-001.wav": {
+    "path": "wav-g-10ths/D3-maj-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 116769,
+      "duration": 2.6478231292517007
+    }
+  },
+  "D3-maj-002.wav": {
+    "path": "wav-g-10ths/D3-maj-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 97531,
+      "duration": 2.2115873015873015
+    }
+  },
+  "D3-maj-003.wav": {
+    "path": "wav-g-10ths/D3-maj-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 67928,
+      "duration": 1.5403174603174603
+    }
+  },
+  "D3-maj-004.wav": {
+    "path": "wav-g-10ths/D3-maj-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 69462,
+      "duration": 1.5751020408163265
+    }
+  },
+  "D3-maj-005.wav": {
+    "path": "wav-g-10ths/D3-maj-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 71282,
+      "duration": 1.6163718820861679
+    }
+  },
+  "D3-maj-006.wav": {
+    "path": "wav-g-10ths/D3-maj-006.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 71269,
+      "duration": 1.6160770975056689
+    }
+  },
+  "E3-min-001.wav": {
+    "path": "wav-g-10ths/E3-min-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 106737,
+      "duration": 2.420340136054422
+    }
+  },
+  "E3-min-002.wav": {
+    "path": "wav-g-10ths/E3-min-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 102016,
+      "duration": 2.3132879818594105
+    }
+  },
+  "E3-min-003.wav": {
+    "path": "wav-g-10ths/E3-min-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 71751,
+      "duration": 1.6270068027210884
+    }
+  },
+  "E3-min-004.wav": {
+    "path": "wav-g-10ths/E3-min-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 64216,
+      "duration": 1.4561451247165533
+    }
+  },
+  "E3-min-005.wav": {
+    "path": "wav-g-10ths/E3-min-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 71979,
+      "duration": 1.6321768707482993
+    }
+  },
+  "E3-min-006.wav": {
+    "path": "wav-g-10ths/E3-min-006.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 79058,
+      "duration": 1.7926984126984127
+    }
+  },
+  "E3-min-007.wav": {
+    "path": "wav-g-10ths/E3-min-007.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 76360,
+      "duration": 1.7315192743764172
+    }
+  },
+  "Fs3-min-001.wav": {
+    "path": "wav-g-10ths/Fs3-min-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 136319,
+      "duration": 3.0911337868480726
+    }
+  },
+  "Fs3-min-002.wav": {
+    "path": "wav-g-10ths/Fs3-min-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 100982,
+      "duration": 2.28984126984127
+    }
+  },
+  "Fs3-min-003.wav": {
+    "path": "wav-g-10ths/Fs3-min-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 67754,
+      "duration": 1.5363718820861678
+    }
+  },
+  "Fs3-min-004.wav": {
+    "path": "wav-g-10ths/Fs3-min-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 114949,
+      "duration": 2.6065532879818596
+    }
+  },
+  "Fs3-min-005.wav": {
+    "path": "wav-g-10ths/Fs3-min-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 73835,
+      "duration": 1.674263038548753
+    }
+  },
+  "G2-p5-000.wav": {
+    "path": "wav-g-10ths/G2-p5-000.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 178330,
+      "duration": 4.043764172335601
+    }
+  },
+  "G3-maj-001.wav": {
+    "path": "wav-g-10ths/G3-maj-001.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 93667,
+      "duration": 2.123968253968254
+    }
+  },
+  "G3-maj-002.wav": {
+    "path": "wav-g-10ths/G3-maj-002.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 98530,
+      "duration": 2.2342403628117915
+    }
+  },
+  "G3-maj-003.wav": {
+    "path": "wav-g-10ths/G3-maj-003.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 66120,
+      "duration": 1.4993197278911565
+    }
+  },
+  "G3-maj-004.wav": {
+    "path": "wav-g-10ths/G3-maj-004.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 142662,
+      "duration": 3.234965986394558
+    }
+  },
+  "G3-maj-005.wav": {
+    "path": "wav-g-10ths/G3-maj-005.wav",
+    "info": {
+      "tagTypes": [],
+      "trackInfo": [],
+      "container": "WAVE",
+      "codec": "PCM",
+      "bitsPerSample": 16,
+      "sampleRate": 44100,
+      "numberOfChannels": 2,
+      "bitrate": 352800,
+      "lossless": true,
+      "numberOfSamples": 71226,
+      "duration": 1.6151020408163266
+    }
+  }
+}
