6cc01ef2873095afdb5fe8b96e5de10ff2a2a099
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-12-10 00:09:14 -0500

    use new "afactory" style tLibs for guitar

---
+++ AFReverse.js
@@ -0,0 +1,81 @@
+
+const { techniques, FluidAudioFile } = require('fluid-music')
+
+/**
+ * @typedef {import('fluid-music').AudioFileOptions} AudioFileOptions
+ * @typedef {import('fluid-music').techniques.AudioFile} AudioFile
+ * @typedef {import('fluid-music').UseContext} UseContext
+ */
+
+class AudioFileRange {
+
+  /**
+   * @param {AudioFile} audioFileTechnique
+   */
+  constructor(audioFileTechnique) {
+    this.audioFile = audioFileTechnique
+  }
+
+  get onsetSeconds () {
+    const onset = this.audioFile.markers.get('onset')
+    return (typeof onset === 'number') ? onset : 0
+  }
+
+  get releaseSeconds () {
+    const release = this.audioFile.markers.get('release')
+    return (typeof release === 'number') ? release : this.audioFile.getSourceDurationSeconds()
+  }
+  /**
+   * @param {UseContext} context 
+   */
+}
+
+class AFReverse extends AudioFileRange {
+  use(context) {
+    const rxFile = this.audioFile.use(context)
+    rxFile.startInSourceSeconds = 0
+    rxFile.playToEnd()
+    rxFile.reverse(true)
+
+    // distance between onset and event end
+    const postOnsetDuration = (rxFile.getSourceDurationSeconds() - this.onsetSeconds) / Math.abs(rxFile.playbackRate)
+    const startSecondsDelta = context.durationSeconds - postOnsetDuration
+    rxFile.startTimeSeconds += startSecondsDelta
+
+    const desiredDuration = context.durationSeconds + (this.onsetSeconds / Math.abs(rxFile.playbackRate))
+    const trimDelta = desiredDuration - rxFile.durationSeconds
+    if (trimDelta < 0) rxFile.growLeftEdgeBySecondsSafe(trimDelta)
+
+    rxFile.mode = FluidAudioFile.Modes.Event
+    rxFile.fadeInSeconds = Math.max(rxFile.fadeInSeconds, 3.5)
+  }
+}
+
+class AFReverseLeadIn extends AudioFileRange {
+  use(context) {
+    const newFile = this.audioFile.use(context)
+
+    newFile.playToEnd()
+    newFile.reverse(true)
+    const leadInSeconds = newFile.getSourcePlaybackSeconds() - this.releaseSeconds
+    newFile.startTimeSeconds -= leadInSeconds
+
+    // prevent lead in from being longer than x seconds
+    const maxLeadInSeconds = 3.5
+    const targetLeadInSeconds = Math.min(maxLeadInSeconds, leadInSeconds)
+    if (targetLeadInSeconds !== leadInSeconds) {
+      newFile.growLeftEdgeBySeconds(maxLeadInSeconds - leadInSeconds)
+    }
+
+    if (newFile.mode === FluidAudioFile.Modes.Event) {
+      newFile.durationSeconds = targetLeadInSeconds + context.durationSeconds
+    } else if (newFile.mode === FluidAudioFile.Modes.OneShot || newFile.mode === FluidAudioFile.Modes.OneVoice) {
+      newFile.playToEnd()
+    }
+  }
+}
+
+module.exports = {
+  AFReverse,
+  AFReverseLeadIn,
+}
--- guitars.js
+++ guitars.js
@@ -2,6 +2,61 @@ const path = require('path')
 const RAlign = require('./RAlign')
 const info10ths = require('./wav-g-10ths')
 const infoStretched = require('./wav-g-10ths-stretch')
+const audioFiles = require('./audio-files')
+const fluid = require('fluid-music')
+const { AFReverse, AFReverseLeadIn } = require('./AFReverse')
+const { AudioFile } = fluid.techniques
+
+const g3rdWaves = audioFiles['wav-g-10ths']
+for (const [basename, audioFile] of Object.entries(g3rdWaves)) {
+  audioFile.fadeInSeconds = 0.05
+  audioFile.fadeOutSeconds = 0.01
+  audioFile.mode = fluid.techniques.AudioFile.Modes.OneVoice
+}
+
+const g3rdSWaves = audioFiles['wav-g-10ths-stretch']
+for (const [basename, audioFile] of Object.entries(g3rdSWaves)) {
+  audioFile.fadeInSeconds = 0.01
+  audioFile.fadeOutSeconds = 3.5
+}
+
+const tLibs = {
+  guitar: {
+    a: AudioFile.copy(g3rdWaves['A2-min-002.wav']),
+    b: AudioFile.copy(g3rdWaves['B2-min-001.wav']),
+    c: AudioFile.copy(g3rdWaves['C3-maj-001.wav']),
+    d: AudioFile.copy(g3rdWaves['D3-maj-001.wav']),
+    e: AudioFile.copy(g3rdWaves['E3-min-001.wav']),
+    f: AudioFile.copy(g3rdWaves['Fs3-min-001.wav']),
+    g: AudioFile.copy(g3rdWaves['G3-maj-001.wav']),
+    h: AudioFile.copy(g3rdWaves['A3-min-004.wav']),
+    i: AudioFile.copy(g3rdWaves['A3-min-001.wav']),
+  },
+  reverse: {},
+  reverseLeadIn: {},
+  stretch: {
+    a: AudioFile.copy(g3rdSWaves['01-A2-min-003-stretch.wav'],  { markers: { onset: 0.0, release: 8.09 } }),   //[45, 45 + 15]),
+    b: AudioFile.copy(g3rdSWaves['02-B2-min-004-stretch.wav'],  { markers: { onset: 0.7, release: 7.47 } }),   //[47, 47 + 15]),
+    c: AudioFile.copy(g3rdSWaves['03-C3-maj-004-stretch.wav'],  { markers: { onset: 0.5, release: 7.5 } }),    //[48, 48 + 16]),
+    d: AudioFile.copy(g3rdSWaves['04-D3-maj-004-stretch.wav'],  { markers: { onset: 0.0, release: 8.2 } }),    //[50, 50 + 16]),
+    e: AudioFile.copy(g3rdSWaves['05-E3-min-005-stretch.wav'],  { markers: { onset: 0.0, release: 7.8 } }),    //[52, 52 + 15]),
+    f: AudioFile.copy(g3rdSWaves['06-Fs3-min-005-stretch.wav'], { markers: { onset: 0.0, release: 7.18 } }),   //[54, 54 + 15]),
+    g: AudioFile.copy(g3rdSWaves['07-G3-maj-002-stretch.wav'],  { markers: { onset: 0.6, release: 15.075 } }), //[55, 55 + 16]),
+    h: AudioFile.copy(g3rdSWaves['08-A3-min-004-stretch.wav'],  { markers: { onset: 0.6, release: 6.29 } }),   //[57, 57 + 15]),
+    i: AudioFile.copy(g3rdSWaves['09-A3-min-001-stretch.wav'],  { markers: { onset: 0.0, release: 9.06 } }),   //[57, 57 + 15]),
+  },
+  stretchReverse: {},
+  stretchReverseLeadIn: {},
+}
+
+// guitar derivative tLibraries
+for (const [key, audioFile] of Object.entries(tLibs.guitar)) tLibs.reverse[key] = new AFReverse(audioFile)
+for (const [key, audioFile] of Object.entries(tLibs.guitar)) tLibs.reverseLeadIn[key] = new AFReverseLeadIn(audioFile)
+// stretch derivative tLibraries
+for (const [key, audioFile] of Object.entries(tLibs.stretch)) tLibs.stretchReverse[key] = new AFReverse(audioFile)
+for (const [key, audioFile] of Object.entries(tLibs.stretch)) tLibs.stretchReverseLeadIn[key] = new AFReverseLeadIn(audioFile)
+
+// Everything below this line uses the old-style sample-scan system. It will be deprecated.
 
 for (const [basename, obj] of Object.entries(info10ths)) {
   obj.path = path.join(__dirname, obj.path)
@@ -38,4 +93,4 @@ const tLibraryStretched = {
   i: new RAlign(infoStretched['09-A3-min-001-stretch.wav'], 0, 9.06,    [57, 57 + 15])
 }
 
-module.exports = { tLibrary, tLibraryStretched }
+module.exports = { tLibrary, tLibraryStretched, tLibs }
