c4d695101cd40dd9e6e522e6fd536f0f0559e332
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-12-14 03:47:37 -0500

    move AFReverse (etc) techniques to `fluid-music`

--- guitars.js
+++ guitars.js
@@ -1,7 +1,6 @@
 const audioFiles = require('./audio-files')
 const fluid = require('fluid-music')
-const { AFReverse, AFReverseLeadIn } = require('./AFReverse')
-const { AudioFile } = fluid.techniques
+const { AFOnset, AFReverse, AFReverseLeadIn } = fluid.techniques
 
 const g3rdWaves = audioFiles['wav-g-10ths']
 for (const [basename, audioFile] of Object.entries(g3rdWaves)) {
@@ -18,38 +17,38 @@ for (const [basename, audioFile] of Object.entries(g3rdSWaves)) {
 
 
 const guitar = {
-  a: AudioFile.copy(g3rdWaves['A2-min-002.wav']),
-  b: AudioFile.copy(g3rdWaves['B2-min-001.wav']),
-  c: AudioFile.copy(g3rdWaves['C3-maj-001.wav']),
-  d: AudioFile.copy(g3rdWaves['D3-maj-001.wav']),
-  e: AudioFile.copy(g3rdWaves['E3-min-001.wav']),
-  f: AudioFile.copy(g3rdWaves['Fs3-min-001.wav']),
-  g: AudioFile.copy(g3rdWaves['G3-maj-001.wav']),
-  h: AudioFile.copy(g3rdWaves['A3-min-004.wav']),
-  i: AudioFile.copy(g3rdWaves['A3-min-001.wav']),
+  a: new AFOnset(g3rdWaves['A2-min-002.wav']),
+  b: new AFOnset(g3rdWaves['B2-min-001.wav']),
+  c: new AFOnset(g3rdWaves['C3-maj-001.wav']),
+  d: new AFOnset(g3rdWaves['D3-maj-001.wav']),
+  e: new AFOnset(g3rdWaves['E3-min-001.wav']),
+  f: new AFOnset(g3rdWaves['Fs3-min-001.wav']),
+  g: new AFOnset(g3rdWaves['G3-maj-001.wav']),
+  h: new AFOnset(g3rdWaves['A3-min-004.wav']),
+  i: new AFOnset(g3rdWaves['A3-min-001.wav']),
 }
 const reverse = {}
 const reverseLeadIn = {}
 
 const stretch = {
-  a: AudioFile.copy(g3rdSWaves['01-A2-min-003-stretch.wav'],  { markers: { onset: 0.0, release: 8.09 } }),   //[45, 45 + 15]),
-  b: AudioFile.copy(g3rdSWaves['02-B2-min-004-stretch.wav'],  { markers: { onset: 0.7, release: 7.47 } }),   //[47, 47 + 15]),
-  c: AudioFile.copy(g3rdSWaves['03-C3-maj-004-stretch.wav'],  { markers: { onset: 0.5, release: 7.5 } }),    //[48, 48 + 16]),
-  d: AudioFile.copy(g3rdSWaves['04-D3-maj-004-stretch.wav'],  { markers: { onset: 0.0, release: 8.2 } }),    //[50, 50 + 16]),
-  e: AudioFile.copy(g3rdSWaves['05-E3-min-005-stretch.wav'],  { markers: { onset: 0.0, release: 7.8 } }),    //[52, 52 + 15]),
-  f: AudioFile.copy(g3rdSWaves['06-Fs3-min-005-stretch.wav'], { markers: { onset: 0.0, release: 7.18 } }),   //[54, 54 + 15]),
-  g: AudioFile.copy(g3rdSWaves['07-G3-maj-002-stretch.wav'],  { markers: { onset: 0.6, release: 15.075 } }), //[55, 55 + 16]),
-  h: AudioFile.copy(g3rdSWaves['08-A3-min-004-stretch.wav'],  { markers: { onset: 0.6, release: 6.29 } }),   //[57, 57 + 15]),
-  i: AudioFile.copy(g3rdSWaves['09-A3-min-001-stretch.wav'],  { markers: { onset: 0.0, release: 9.06 } }),   //[57, 57 + 15]),
+  a: new AFOnset(g3rdSWaves['01-A2-min-003-stretch.wav'], 0.0, 8.09),   //[45, 45 + 15]),
+  b: new AFOnset(g3rdSWaves['02-B2-min-004-stretch.wav'], 0.7, 7.47),   //[47, 47 + 15]),
+  c: new AFOnset(g3rdSWaves['03-C3-maj-004-stretch.wav'], 0.5, 7.5),    //[48, 48 + 16]),
+  d: new AFOnset(g3rdSWaves['04-D3-maj-004-stretch.wav'], 0.0, 8.2),    //[50, 50 + 16]),
+  e: new AFOnset(g3rdSWaves['05-E3-min-005-stretch.wav'], 0.0, 7.8),    //[52, 52 + 15]),
+  f: new AFOnset(g3rdSWaves['06-Fs3-min-005-stretch.wav'],0.0, 7.18),   //[54, 54 + 15]),
+  g: new AFOnset(g3rdSWaves['07-G3-maj-002-stretch.wav'], 0.6, 15.075), //[55, 55 + 16]),
+  h: new AFOnset(g3rdSWaves['08-A3-min-004-stretch.wav'], 0.6, 6.29),   //[57, 57 + 15]),
+  i: new AFOnset(g3rdSWaves['09-A3-min-001-stretch.wav'], 0.0, 9.06),   //[57, 57 + 15]),
 }
 const stretchReverse = {}
 const stretchReverseLeadIn = {}
 
 // guitar derivative tLibraries
-for (const [key, audioFile] of Object.entries(guitar)) reverse[key] = new AFReverse(audioFile)
-for (const [key, audioFile] of Object.entries(guitar)) reverseLeadIn[key] = new AFReverseLeadIn(audioFile)
+for (const [key, afOnset] of Object.entries(guitar)) reverse[key] = new AFReverse(afOnset.audioFile)
+for (const [key, afOnset] of Object.entries(guitar)) reverseLeadIn[key] = new AFReverseLeadIn(afOnset.audioFile)
 // stretch derivative tLibraries
-for (const [key, audioFile] of Object.entries(stretch)) stretchReverse[key] = new AFReverse(audioFile)
-for (const [key, audioFile] of Object.entries(stretch)) stretchReverseLeadIn[key] = new AFReverseLeadIn(audioFile)
+for (const [key, afOnset] of Object.entries(stretch)) stretchReverse[key] = new AFReverse(afOnset.audioFile)
+for (const [key, afOnset] of Object.entries(stretch)) stretchReverseLeadIn[key] = new AFReverseLeadIn(afOnset.audioFile)
 
 module.exports = { guitar, reverse, reverseLeadIn, stretch, stretchReverse, stretchReverseLeadIn }
--- rides.js
+++ rides.js
@@ -1,5 +1,6 @@
+const fluid = require('fluid-music')
+const { AFOnset, AFReverse, AFReverseLeadIn } = fluid.techniques
 
-const { AFReverse, AFReverseLeadIn } = require('./AFReverse')
 const audioFiles = require('./audio-files')
 const rideFiles = audioFiles.rides
 
@@ -8,21 +9,21 @@ rideFiles['R&B Ride 02 - Close.wav'].gainDb = 12
 rideFiles['R&B Ride Bell 01 - Close.wav'].gainDb = -5
 
 const rides = {
-  a: rideFiles['Jazz Ride Bell 01 - Close.wav'],
-  b: rideFiles['Jazz Hi-Hat 10 - Close.wav'],  // played like a ride
-  c: rideFiles['Jazz Ride 02 - Room.wav'],
-  d: rideFiles['R&B Ride 01 - Room.wav'],
+  a: new AFOnset(rideFiles['Jazz Ride Bell 01 - Close.wav']),
+  b: new AFOnset(rideFiles['Jazz Hi-Hat 10 - Close.wav']),  // played like a ride
+  c: new AFOnset(rideFiles['Jazz Ride 02 - Room.wav']),
+  d: new AFOnset(rideFiles['R&B Ride 01 - Room.wav']),
   // Mono Rides
-  A: rideFiles['R&B Ride 01 - Close.wav'],
-  B: rideFiles['R&B Ride 02 - Close.wav'],
-  C: rideFiles['R&B Ride Bell 01 - Close.wav'],
-  D: rideFiles['R&B Ride Bell 02 - Close.wav'],
+  A: new AFOnset(rideFiles['R&B Ride 01 - Close.wav']),
+  B: new AFOnset(rideFiles['R&B Ride 02 - Close.wav']),
+  C: new AFOnset(rideFiles['R&B Ride Bell 01 - Close.wav']),
+  D: new AFOnset(rideFiles['R&B Ride Bell 02 - Close.wav']),
 }
 
 const ridesReverse = {}
-for (const [key, audioFile] of Object.entries(rides)) ridesReverse[key] = new AFReverse(audioFile)
+for (const [key, afOnset] of Object.entries(rides)) ridesReverse[key] = new AFReverse(afOnset.audioFile)
 
 const ridesReverseLeadIn = {}
-for (const [key, audioFile] of Object.entries(rides)) ridesReverseLeadIn[key] = new AFReverseLeadIn(audioFile)
+for (const [key, afOnset] of Object.entries(rides)) ridesReverseLeadIn[key] = new AFReverseLeadIn(afOnset.audioFile)
 
 module.exports = { rides, ridesReverse, ridesReverseLeadIn }
